1abcc8c21015c57eb52fe5e1e01a37e14dca05a1
SamRamdhan <69948351+SamRamdhan@users.noreply.github.com>
2021-01-06 15:31:01 +0800

    Update fun.py

--- SaitamaRobot/modules/fun.py
+++ SaitamaRobot/modules/fun.py
@@ -44,7 +44,7 @@ def slap(update: Update, context: CallbackContext):
 
     reply_text = message.reply_to_message.reply_text if message.reply_to_message else message.reply_text
 
-    curr_user = "@" + html.escape(mention_markdown(message.from_user.id, message.from_user.first_name))
+    curr_user = "{}". format(mention_html(message.from_user.id, message.from_user.first_name))
     user_id = extract_user(message, args)
 
     if user_id == bot.id:
@@ -71,10 +71,10 @@ def slap(update: Update, context: CallbackContext):
 
         slapped_user = bot.get_chat(user_id)
         user1 = curr_user
-        user2 = "{}".format(mention_markdown(slapped_user.id, slapped_user.first_name))
+        user2 = "{}".format(mention_html(slapped_user.id, slapped_user.first_name))
 
     else:
-        user1 = "{}".format(mention_markdown(context.bot.id, context.bot.first_name))
+        user1 = "{}".format(mention_html(context.bot.id, context.bot.first_name))
         user2 = curr_user
 
     temp = random.choice(fun_strings.SLAP_TEMPLATES)
