37dd6290e8e3fdd8446b809578fd4206b1d37987
SamRamdhan <69948351+SamRamdhan@users.noreply.github.com>
2021-01-15 04:32:09 +0800

    Update users.py

--- SaitamaRobot/modules/users.py
+++ SaitamaRobot/modules/users.py
@@ -148,11 +148,11 @@ def chat_checker(update: Update, context: CallbackContext):
 
 def __user_info__(user_id):
     if user_id in [777000, 1087968824]:
-        return """╘══「 Groups count: <code>???</code> 」"""
+        return """╘══「 Jumlah grup: <code>???</code> 」"""
     if user_id == dispatcher.bot.id:
-        return """╘══「 Groups count: <code>???</code> 」"""
+        return """╘══「 Jumlah grup: <code>???</code> 」"""
     num_chats = sql.get_user_num_chats(user_id)
-    return f"""╘══「 Groups count: <code>{num_chats}</code> 」"""
+    return f"""╘══「 Jumlah grup: <code>{num_chats}</code> 」"""
 
 
 def __stats__():
